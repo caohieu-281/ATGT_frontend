8c9a9f4873a69b2bc08180f15665d8b47db351b5
dangcuong-123 <cuong.dang@pionero.io>
2022-12-16 12:46:59 +0700

    update camera

--- src/screen/CameraScreen.tsx
+++ src/screen/CameraScreen.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactPlayer from "react-player";
 
 export default function CameraScreen() {
-  const url = "https://www.youtube.com/watch?v=OoQpHELxIwc";
+  const url = "rtsp://118.70.32.198:80/Streaming/Channels/101";
   return (
     <div>
       <h1 className="text-xl">Hello world!</h1>
       <ReactPlayer
+        playing={true}
         url={url}
         width="100vh"
         height="100vh"
