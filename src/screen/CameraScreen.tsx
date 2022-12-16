336a4cd1df9b9fa2cd2f34d363d6829c0fb34266
dangcuong-123 <cuong.dang@pionero.io>
2022-12-16 09:18:08 +0700

    update camera

--- src/screen/CameraScreen.tsx
+++ src/screen/CameraScreen.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 export default function CameraScreen() {
-  return <div></div>;
+  const url = "https://www.youtube.com/watch?v=OoQpHELxIwc";
+  return (
+    <div>
+      <h1 className="text-xl">Hello world!</h1>
+      <ReactPlayer
+        url={url}
+        width="100vh"
+        height="100vh"
+        config={{
+          youtube: {
+            playerVars: { showinfo: 1 },
+          },
+          facebook: {
+            appId: "12345",
+          },
+        }}
+      />
+    </div>
+  );
 }
