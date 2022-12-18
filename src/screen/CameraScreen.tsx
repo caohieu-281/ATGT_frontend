448aca449ec35864b9eb94ca4f050b51ad29edb1
caohieu-281 <kvlingxss@gmail.com>
2022-12-18 15:28:28 +0700

    camera screen

--- src/screen/CameraScreen.tsx
+++ src/screen/CameraScreen.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import ReactDOM from "react-dom";
 import MultipleCanvas from "../components/DrawPolygon/MultipleCanvas";
 
 export default function CameraScreen() {
   const url = "https://www.youtube.com/watch?v=urZ0bhF9bB4";
   return (
-    <div>
-      <ReactPlayer
-        // playing={true}
-        url={url}
-        // loop="true"
-        width="100vh"
-        height="100vh"
-        config={{
-          youtube: {
-            playerVars: { showinfo: 1 },
-          },
-          facebook: {
-            appId: "12345",
-          },
-        }}
-      />
+    <div className="container-fluid m-0 pt-5" style={container}>
+      <div style={formatCam}>
+        <ReactPlayer
+          // playing={true}
+          url={url}
+          width="100vh"
+          height="60vh"
+          config={{
+            youtube: {
+              playerVars: { showinfo: 1 },
+            },
+            facebook: {
+              appId: "12345",
+            },
+          }}
+        />
+      </div>
       <MultipleCanvas />
     </div>
   );
 }
+
+const container = {
+  // marginTop:0,
+  backgroundColor: "aliceblue",
+  // paddingTop:20,
+};
+
+const formatCam = {
+  display: "flex",
+  justifyContent: "center",
+  
+};
