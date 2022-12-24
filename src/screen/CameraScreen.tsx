a01a8ad8028ff61e34cce11b2f3089466f5658a7
caohieu-281 <kvlingxss@gmail.com>
2022-12-24 23:48:06 +0700

    add noti camera screen

--- src/screen/CameraScreen.tsx
+++ src/screen/CameraScreen.tsx
@@ -3,13 +3,27 @@ import ReactPlayer from "react-player";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import MultipleCanvas from "../components/DrawPolygon/MultipleCanvas";
 // import Button from "../components/ButtonConva/Button";
-import { Modal } from "antd";
+import { Modal, notification } from "antd";
+// import SendImgViaTelegram from "../bot/SendImgViaTelegram";
+
 export default function CameraScreen() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isPlay, setIsPlay] = useState(false);
   const [timeAppear1, setTimeAppear1] = useState(false);
   const [timeAppear2, setTimeAppear2] = useState(false);
   const [timeAppear3, setTimeAppear3] = useState(false);
+  // const [api, contextHolder] = notification.useNotification();
+
+  const openNotification = () => {
+    notification.warning({
+      message: "Xuát hiện vi phạm",
+      description: "Phát hiện phương tiện vi phạm. Đã gửi về hệ thống",
+      duration: 3,
+      style: {
+        width: 430,
+      },
+    });
+  };
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -30,18 +44,27 @@ export default function CameraScreen() {
     setTimeout(function () {
       setTimeAppear1(true);
     }, 2000);
+    // SendImgViaTelegram(
+    //   "https://drive.google.com/file/d/1DzkmA3_kSWSRzPBhDEKWQkf_k1vpoD11/view?usp=sharing"
+    // );
   };
 
   const createViPham2 = () => {
     setTimeout(() => {
       setTimeAppear2(true);
     }, 5000);
+    // SendImgViaTelegram(
+    //   "https://drive.google.com/file/d/1q0Me1WhqO2aWaOUfI111fM50Hl3gKvco/view?usp=sharing"
+    // );
   };
 
   const createViPham3 = () => {
     setTimeout(() => {
       setTimeAppear3(true);
     }, 7000);
+    // SendImgViaTelegram(
+    //   "https://drive.google.com/file/d/1q0Me1WhqO2aWaOUfI111fM50Hl3gKvco/view?usp=sharing"
+    // );
   };
 
   const viPham1 = {
@@ -49,7 +72,7 @@ export default function CameraScreen() {
     image: "img/demo_result.jpg",
   };
   const viPham2 = {
-    time: "16::50:01",
+    time: "16:50:01",
     image: "img/demo_result.jpg",
   };
   const viPham3 = {
@@ -72,9 +95,22 @@ export default function CameraScreen() {
     confirm();
   }, []);
 
+  useEffect(() => {
+    if (timeAppear1 && !timeAppear2) {
+      openNotification();
+    }
+    if (timeAppear2 && !timeAppear3) {
+      openNotification();
+    }
+    if (timeAppear3) {
+      openNotification();
+    }
+  }, [timeAppear1, timeAppear2, timeAppear3]);
+
   return (
     <div className="w-100 container-fluid mt-3 mb-5">
       <>
+        {/* {contextHolder} */}
         <Modal
           title="Thêm vùng vi phạm"
           open={isModalOpen}
@@ -87,7 +123,7 @@ export default function CameraScreen() {
           <MultipleCanvas />
         </Modal>
       </>
-      <div className="row text-center  mb-3">
+      <div className="row text-center text-lg mb-3">
         <h1 className="">Khu vực: Nguyễn Chí Thanh</h1>
       </div>
       <div className="row justify-content-center px-2">
@@ -115,13 +151,14 @@ export default function CameraScreen() {
           className="col-5"
           style={{
             boxShadow: ".5px .5px 5px .4em rgba(0,0,0,.1)",
+            height: "80vh",
           }}
         >
           <h3 className="text-center pt-3 pb-2">
             Những phương tiện vi phạm giao thông
           </h3>
           {/* list */}
-          <div className="overflow-auto" style={{ height: "600px" }}>
+          <div className="overflow-auto" style={{ height: "90%" }}>
             {timeAppear1 && (
               <>
                 <div
